2ea158634dcb40d5b5d8657572ddde66a1876d83
mtoutcalt <mtoutcalt@gmail.com>
2017-04-12 21:10:11 -0400

    -using thread runnable and handler for breathe activity to get timing right

--- app/src/main/java/foci/bu/outcalt/fociapp/calm/BreatheActivity.java
+++ app/src/main/java/foci/bu/outcalt/fociapp/calm/BreatheActivity.java
@@ -3,6 +3,8 @@ package foci.bu.outcalt.fociapp.calm;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,6 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import foci.bu.outcalt.fociapp.R;
 import foci.bu.outcalt.fociapp.habit.HabitActivity;
@@ -23,6 +29,28 @@ public class BreatheActivity extends AppCompatActivity {
     int counter = 1;
     TextView textView;
 
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String string = bundle.getString("displayString");
+            TextView myTextView = (TextView) findViewById(R.id.breathText);
+            myTextView.setText(string);
+            textView.postDelayed(updateText, 3875);
+        }
+    };
+
+    Runnable updateText = new Runnable () {
+        public void run() {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            String string = determineTextChange();
+            bundle.putString("displayString", string);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,34 +63,26 @@ public class BreatheActivity extends AppCompatActivity {
         ImageView mImageViewFilling = (ImageView) findViewById(R.id.animate_breathe);
         ((AnimationDrawable) mImageViewFilling.getBackground()).start();
 
-        textView.postDelayed(updateText, 3800);
+        textView.postDelayed(updateText, 4000);
     }
 
-    Runnable updateText = new Runnable () {
-        public void run() {
-            determineTextChange();
-            textView.postDelayed(updateText, 3800);
-        }
-    };
-
-    private void determineTextChange() {
+    private String determineTextChange() {
         switch (counter) {
-            case 1: textView.setText("Hold");
+            case 1:
                 counter++;
-                break;
-            case 2: textView.setText("BREATHE OUT");
+                return "Hold";
+            case 2:
                 counter++;
-                break;
-            case 3: textView.setText("HOLD");
+                return "BREATHE OUT";
+            case 3:
                 counter++;
-                break;
-            case 4: textView.setText("BREATHE");
+                return "HOLD";
+            case 4:
                 counter = 1;
-                break;
-            default: textView.setText("ERROR");
+                return "BREATHE IN";
+            default: return "ERROR";
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
